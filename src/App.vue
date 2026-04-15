e111aef9e76935f530b4ecd5fb5071c91943192b
LaluBear <nattapat969@gmail.com>
2026-04-16 00:59:33 +0900

    Add Readme.md

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,14 @@
 <template>
   <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
     <SiteNavigation />
-    <RouterView v-slot="{ Component }">
-      <Transition name="page" mode="out-in">
+
+    <RouterView v-slot="{ Component }" class="p-4 w-full">
+      <Transition name="page" mode="out-in" class="p-4 w-full">
         <Component :is="Component"/>
       </Transition>
 
     </RouterView>
   </div>
-  <h1>Hello World</h1>
 </template>
 
 <script setup lang="ts">
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,15 +7,15 @@
       placeholder="Search for a city or state"
       v-model="searchQuery"
       @input="getSearchResults"
-      class="py-2 px-1 w-full bg-transparent border-b 
+      class="py-2 px-4 w-full bg-transparent border-b 
       focus:border-weather-secondary focus:outline-none
       focus:shadow-[0px_1px_0_0_#004E71]">
       <ul class="absolute bg-weather-secondary text-white 
-      w-full shadow-md py-2 px-1 top-[66px]">
+      w-full shadow-md py-2 px-4 top-[66px]">
         <p v-if="searchError">
           Sorry, something went wrong, please try again
         </p>
-        <p v-if="mapboxSearchResults && !searchError && mapboxSearchResults.length ===0">
+        <p v-if="mapboxSearchResults && !searchError && mapboxSearchResults.length === 0">
           No search results
         </p>
         <li 
